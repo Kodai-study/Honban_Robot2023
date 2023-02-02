2949cb3c6298bff29c5a9836969d9dac21cb2ddb
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2023-02-02 17:39:49 +0900

    テーブル表示変更のサンプル
    ボタンを押すと、対応したテーブルの列の表示、非表示を切り替える

--- app/src/main/java/com/example/honban_robot2023/resultTable_Activity.java
+++ app/src/main/java/com/example/honban_robot2023/resultTable_Activity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
@@ -29,36 +31,56 @@ public class resultTable_Activity extends AppCompatActivity {
      */
     final TableLayout.LayoutParams rowLayoutParams = new TableLayout.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
             TableRow.LayoutParams.WRAP_CONTENT);
-    /**
-     * テーブルの行の、1つ目の要素に適応させるレイアウトデータ
-     */
-    final TableRow.LayoutParams firstTextLayoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
-            TableRow.LayoutParams.WRAP_CONTENT);
 
-    /**
-     * テーブルの行の、1つ目以降の要素に適応させるレイアウトデータ
-     */
-    final TableRow.LayoutParams textLayoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
-            TableRow.LayoutParams.WRAP_CONTENT);
+    boolean[] viewable = new boolean[]{true, true, true, true};
+
+    Button[] button = new Button[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_table);
 
-        //rowLayoutParams.topMargin = 100;
-        rowLayoutParams.setMargins(10,0,10,10);
-
+        rowLayoutParams.setMargins(10, 0, 10, 10);
 
         resultTable = findViewById(R.id.table_resultDatas);
         resultTable.getChildAt(0).setBackgroundColor(Color.GRAY);
         resultTable.setBackgroundColor(Color.WHITE);
+
+        button[0] = findViewById(R.id.button);
+        button[1] = findViewById(R.id.button2);
+        button[2] = findViewById(R.id.button3);
+        button[3] = findViewById(R.id.button4);
+        for (int i = 0; i < button.length; i++) {
+            button[i].setOnClickListener(new ColumButtonClickListener(i));
+        }
         fetchAPISample();
     }
 
+    class ColumButtonClickListener implements View.OnClickListener {
+
+        private int columPosition;
+
+        public ColumButtonClickListener(int columPosition) {
+            this.columPosition = columPosition;
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            for (int i = 0; i < resultTable.getChildCount(); i++) {
+                TableRow tableRow = (TableRow) (resultTable.getChildAt(i));
+                if (viewable[columPosition]) {
+                    tableRow.getChildAt(columPosition).setVisibility(View.GONE);
+                } else {
+                    tableRow.getChildAt(columPosition).setVisibility(View.VISIBLE);
+                }
+            }
+            viewable[columPosition] = !viewable[columPosition];
+        }
+    }
 
     /**
-     *
      * @see <a href="https://jsonplaceholder.typicode.com/posts">公開されているサンプルのWebAPI</a>
      * からJSONデータを持ってきてTableLayoutに表示する。
      */
@@ -84,14 +106,14 @@ public class resultTable_Activity extends AppCompatActivity {
                     TableRow t = new TableRow(resultTable_Activity.this);
 
                     RowDataViews tableRowViews = new RowDataViews
-                            (resultTable_Activity.this,e);
+                            (resultTable_Activity.this, e);
 
                     t.addView(tableRowViews.getUserIdText());
                     t.addView(tableRowViews.getIdText());
                     t.addView(tableRowViews.getTitleText());
                     t.addView(tableRowViews.getSubTitleText());
                     t.setLayoutParams(rowLayoutParams);
-                    t.setPadding(0,100,0,0);
+                    t.setPadding(0, 100, 0, 0);
                     t.setBackgroundColor(Color.BLACK);
                     resultTable.addView(t);
                 }
