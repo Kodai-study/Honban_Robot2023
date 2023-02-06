1327a7421d1343dd4c12db16fcd083be3e632211
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2023-02-06 19:50:35 +0900

    ステーション全体の状況を表示する画面を適当に作成
    アイコンとメニューも作ってみたよ

--- app/src/main/java/com/example/honban_robot2023/MainActivity.java
+++ app/src/main/java/com/example/honban_robot2023/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.honban_robot2023;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.mainpage_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
