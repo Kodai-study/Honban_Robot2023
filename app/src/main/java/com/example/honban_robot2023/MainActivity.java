92b88202d7f0eaea05f44004b34fa5d2cec16ade
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2023-02-17 23:03:22 +0900

    メインアクティビティから、ボタンとメニューで他のテーブル画面に移動するように

--- app/src/main/java/com/example/honban_robot2023/MainActivity.java
+++ app/src/main/java/com/example/honban_robot2023/MainActivity.java
@@ -3,16 +3,51 @@ package com.example.honban_robot2023;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button result;
+    Button Statistics;
+    Button TimeInterval;
+    Button Utilization;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.button_moveResultTable);
+        Statistics = findViewById(R.id.button_moveStatistics);
+        TimeInterval = findViewById(R.id.button_moveTimeInterval);
+        Utilization = findViewById(R.id.button_moveUtilization);
+
+        result.setOnClickListener(new clickToMove(ResultTable_Activity.class));
+        Statistics.setOnClickListener(new clickToMove(StatisticsTable_Activity.class));
+        TimeInterval.setOnClickListener(new clickToMove(TimeIntervalsTable_Activity.class));
+        Utilization.setOnClickListener(new clickToMove(UtilizationTable_Activity.class));
+    }
+
+    class clickToMove implements View.OnClickListener {
+
+        Class activity;
+
+        public clickToMove(Class activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, activity);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -23,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (R.id.menu_moveToResultTable == item.getItemId())
+            startActivity(new Intent(MainActivity.this, ResultTable_Activity.class));
+        else if (R.id.menu_moveStatisticsTable == item.getItemId())
+            startActivity(new Intent(MainActivity.this, StatisticsTable_Activity.class));
+        else if (R.id.menu_moveToTimeTable == item.getItemId())
+            startActivity(new Intent(MainActivity.this, TimeIntervalsTable_Activity.class));
+        else if (R.id.menu_moveToUtilizationTable == item.getItemId())
+            startActivity(new Intent(MainActivity.this, UtilizationTable_Activity.class));
+
         return super.onOptionsItemSelected(item);
     }
 
