f00706d10ffc0bc168a7f66a71fa385e4536a873
Kodai-study <anchor.kou@softbank.ne.jp>
2023-02-21 22:07:53 +0900

    アプリケーションとしての操作や画面遷移のフローを見直す
    タイトル画面と、分析情報を選択する画面の初期バージョンを実現

---
+++ app/src/main/java/com/example/honban_robot2023/AnalysisSelection_Activity.java
@@ -0,0 +1,14 @@
+package com.example.honban_robot2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnalysisSelection_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis_selection);
+    }
+}
