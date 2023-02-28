02d5247ba343606e19b332a14a9014dda2c9259c
Tsukurimichi <user@RBPC12>
2023-02-28 16:33:44 +0900

    カードビューのレイアウトの作成

---
+++ app/src/main/java/com/example/honban_robot2023/Test/sample0228.java
@@ -0,0 +1,16 @@
+package com.example.honban_robot2023.Test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.honban_robot2023.R;
+
+public class sample0228 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_sample);
+    }
+}
